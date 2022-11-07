f49222fcd0698510a85d32f8ad6220305cbba5a1
youminlim <lim.ym5327@gmail.com>
2022-11-07 17:56:53 +0000

    fixed function definition for acceleration checker, formula was missing a negative sign

--- SpaceProjects/2body/myFunctions.jl
+++ SpaceProjects/2body/myFunctions.jl
@@ -78,6 +78,7 @@ function rkSolver(rk, f, t0, tf, y0, n)
     return t, y
 end
 
+
 # Physics Checker
 function gravity(G, m, y)
     # Calculates the gravitational force experienced by a body
@@ -86,12 +87,13 @@ function gravity(G, m, y)
     return (G * m) / r^2
 end
 
+
 function accelChecker(G, m1, m2, y)
     # Checks if the acceleration forces experienced by the bodies are of correct magnitude
     
     dims = size(y)
     for i in range(1, length = dims[1])
-        if gravity(G, m1, y[i, :]) != (m1 / m2) * gravity(G, m2, y[i, :])
+        if gravity(G, m1, y[i, :]) != -(m1 / m2) * gravity(G, m2, y[i, :])
          println("Error!")
         end 
      end 
